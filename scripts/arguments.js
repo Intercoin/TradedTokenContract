a5434d36d5ae2a82b29572cd242461433021ac81
Artem Subbotin <subbotin.artem@gmail.com>
2022-10-20 11:25:48 +0300

    stashed

--- scripts/arguments.js
+++ scripts/arguments.js
@@ -1,7 +1,8 @@
 module.exports =  [
   'Intercoin',
   'ITR',
-  '0x9fb83c0635de2e815fd1c21b3a292277540c2e8d',
+  '0xe9e7cea3dedca5984780bafc599bd69add087d56',   // bsc
+  //'0x9fb83c0635de2e815fd1c21b3a292277540c2e8d', // matic
   1000,
   365,
   [ 1, 10 ],
