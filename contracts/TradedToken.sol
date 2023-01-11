4a4f43a19b13a51643bed519a45933b5249a6429
Artem Subbotin <subbotin.artem@gmail.com>
2023-01-11 13:48:02 +0200

    copy erc777 from openzeppelin folder

--- contracts/TradedToken.sol
+++ contracts/TradedToken.sol
@@ -2,7 +2,8 @@
 pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+//import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+import "./helpers/ERC777.sol";
 import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
 import "@openzeppelin/contracts/token/ERC777/IERC777Sender.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
