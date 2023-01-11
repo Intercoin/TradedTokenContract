2349b35ae1e5efc23ffa389c2fbc5ee7a22a0768
Artem Subbotin <subbotin.artem@gmail.com>
2023-01-11 17:48:23 +0200

    removed custom files

--- contracts/mocks/IUniswapCustomMock.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
-
-interface IUniswapCustomMock {
-    function sync() external;
-    function price1CumulativeLast() external view returns (uint);
-}
