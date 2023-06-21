a52b977a50c82f0cea58be649be76555d1f75ce4
Artem Subbotin <subbotin.artem@gmail.com>
2023-06-21 17:49:39 +0300

    TTT-10 | Condition is never met

--- contracts/TradedToken.sol
+++ contracts/TradedToken.sol
@@ -703,10 +703,7 @@ contract TradedToken is Ownable, IClaim, IERC777Recipient, IERC777Sender, ERC777
         
         uint256 currentBalance = balanceOf(holder);
 
-        uint32 duration = panicSellRateLimit.duration;
-        duration = (duration == 0) ? 1 : duration; // make no sense if duration eq 0      
-
-        if (block.timestamp / duration * duration > _buckets[holder].lastBucketTime) {
+        if (block.timestamp / panicSellRateLimit.duration * panicSellRateLimit.duration > _buckets[holder].lastBucketTime) {
             _buckets[holder].lastBucketTime = uint64(block.timestamp);
             _buckets[holder].remainingToSell = currentBalance * panicSellRateLimit.fraction / FRACTION;
         }
