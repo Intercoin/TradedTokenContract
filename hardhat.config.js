1c98507b627e9b1e6ec59f5ce737a9964630a5ac
Artem Subbotin <subbotin.artem@gmail.com>
2023-11-27 14:48:06 +0200

    solidity compiler up to 0.8.23

--- hardhat.config.js
+++ hardhat.config.js
@@ -141,7 +141,7 @@ module.exports = {
   solidity: {
     compilers: [
         {
-          version: "0.8.18",
+          version: "0.8.23",
           settings: {
             optimizer: {
               enabled: true,
